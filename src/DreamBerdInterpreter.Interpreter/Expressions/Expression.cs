f1f203f9336e8a16843a1836575ac3c4c62f94ff
lucaaaum <lucasrenato.fernandesdasilva@gmail.com>
2024-05-01 20:07:54 -0300

    Creating constructor for Expression class

--- src/DreamBerdInterpreter.Interpreter/Expressions/Expression.cs
+++ src/DreamBerdInterpreter.Interpreter/Expressions/Expression.cs
@@ -1,8 +1,8 @@
 namespace DreamBerdInterpreter.Interpreter.Expressions;
 
-public class Expression
+public class Expression(string content, bool dumbProgrammerWantsToDebug, int priority)
 {
-    public string Content { get; init; }
-    public bool DumbProgrammerWantsToDebug { get; init; }
-    public int Priority { get; init; }
+    public string Content { get; init; } = content;
+    public bool DumbProgrammerWantsToDebug { get; init; } = dumbProgrammerWantsToDebug;
+    public int Priority { get; init; } = priority;
 }
