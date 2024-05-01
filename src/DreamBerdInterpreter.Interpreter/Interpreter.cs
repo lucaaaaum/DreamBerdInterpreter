1726f1bccdc6fa77277f8e2e6ab39035ca31d6ef
lucaaaum <lucasrenato.fernandesdasilva@gmail.com>
2024-05-01 12:48:04 -0300

    Creating an interpreter class and it's testing class

---
+++ src/DreamBerdInterpreter.Interpreter/Interpreter.cs
@@ -0,0 +1,9 @@
+namespace DreamBerdInterpreter.Interpreter;
+
+public static class Interpreter
+{
+    public static void Interpret(string fileContent)
+    {
+        
+    }
+}
---
+++ test/DreamBerdInterpreter.Interpreter.Tests/DreamBerdInterpreter.Interpreter.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace DreamBerdInterpreter.Interpreter.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
