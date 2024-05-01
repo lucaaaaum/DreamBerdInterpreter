1f9fbbf354e67958673d2de09cf6371f90601d43
lucaaaum <lucasrenato.fernandesdasilva@gmail.com>
2024-05-01 20:10:15 -0300

    Creating string extension method to multiply a string

--- src/DreamBerdInterpreter.Interpreter/Extensions/StringExtensions.cs
+++ src/DreamBerdInterpreter.Interpreter/Extensions/StringExtensions.cs
@@ -1,3 +1,4 @@
+using System.Text;
 using System.Text.RegularExpressions;
 
 namespace DreamBerdInterpreter.Interpreter.Extensions;
@@ -7,4 +8,12 @@ public static class StringExtensions
     public static bool Matches(this string text, Regex regex) => regex.IsMatch(text);
 
     public static IEnumerable<string> SplitAccordingTo(this string text, Regex regex) => regex.Split(text);
+
+    public static string Times(this string text, int amount)
+    {
+        var result = string.Empty;
+        for (var count = 0; count < amount; count++)
+            result += text;
+        return result;
+    }
 }
