34ac54cb0aaec6c9ab8bef7fc292b83347c19f2f
lucaaaum <lucasrenato.fernandesdasilva@gmail.com>
2024-05-01 16:28:21 -0300

    Creating VarConst to abstract variables and constants

---
+++ src/DreamBerdInterpreter.Interpreter/Types/VarConst.cs
@@ -0,0 +1,25 @@
+namespace DreamBerdInterpreter.Interpreter.Types;
+
+public class VarConst
+{
+    public string Name { get; init; }
+    private string _value;
+    public string Value
+    {
+        get => _value;
+        set => _value = CanBeAssigned 
+            ? value 
+            : throw new Exception("bro you can't assign a new to value to a variable that cannot be re-assigned that's like so dumb go read the docs");
+    }
+    public bool CanBeEdited { get; init; }
+    public bool CanBeAssigned { get; init; }
+
+    public VarConst(string name, bool canBeEdited, bool canBeAssigned)
+    {
+        Name = name;
+        CanBeEdited = canBeEdited;
+        CanBeAssigned = canBeAssigned;
+    }
+
+    public Dictionary<string, VarConst> Definitions { get; init; }
+}
