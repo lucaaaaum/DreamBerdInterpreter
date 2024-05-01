c84255670042fdfc1e5cf8e1dedabd993ec01ec5
lucaaaum <lucasrenato.fernandesdasilva@gmail.com>
2024-05-01 18:03:48 -0300

    Creating code for the declaration of variables

--- src/DreamBerdInterpreter.Interpreter/Interpreter.cs
+++ src/DreamBerdInterpreter.Interpreter/Interpreter.cs
@@ -1,29 +1,51 @@
 using System.Text.RegularExpressions;
 using DreamBerdInterpreter.Infrastructure.Console;
+using DreamBerdInterpreter.Interpreter.Extensions;
+using DreamBerdInterpreter.Interpreter.Types;
 
 namespace DreamBerdInterpreter.Interpreter;
 
 public class Interpreter(IConsole console)
 {
     private readonly IConsole _console = console;
-    private IDictionary<string, string> _variables = new Dictionary<string, string>();
+    private IDictionary<string, VarConst> _variables = new Dictionary<string, VarConst>();
     private IDictionary<string, string> _functions = new Dictionary<string, string>();
 
     private Regex expressionSplitRegex = new(@"([!?]+)(?![^{]*})|}", RegexOptions.Compiled);
-    private Regex onlyHasExclamationPointsAndQuestionMarksRegex = new("^[!?]+$");
-    private Regex varConstRegex = new(@"(var|const)\s*(var|const)\s*([^\s]*)", RegexOptions.Compiled);
+    private Regex onlyHasExclamationPointsAndQuestionMarksRegex = new("^[!?]+$", RegexOptions.Compiled);
+    private Regex varConstRegex = new(@"(var|const)\s*(var|const)\s*([^\s]*)\s*=*\s*([^!?]*)", RegexOptions.Compiled);
 
-    public void Interpret(string expression)
+    public string Interpret(string expression, int visibilityLevel = 0)
     {
         var subExpressions = GetSubExpressions(expression);
+
+        foreach (var subExpression in subExpressions)
+            if (subExpression.Matches(varConstRegex))
+            {
+                var variable = GetVarConstFromSubExpression(visibilityLevel, subExpression);
+                _variables.Add(variable.Name, variable);
+            }
+
+        return default;
+    }
+
+    private VarConst GetVarConstFromSubExpression(int visibilityLevel, string subExpression)
+    {
+        var match = varConstRegex.Match(subExpression);
+        var reassignable = match.Groups[1].ToString() == "var";
+        var editable = match.Groups[2].ToString() == "var";
+        var name = match.Groups[3].ToString();
+        var value = match.Groups[4].ToString();
+        var variable = new VarConst(value, name, editable, reassignable, visibilityLevel);
+        return variable;
     }
 
     private IEnumerable<string> GetSubExpressions(string expression)
     {
-        var splittedExpressions = expressionSplitRegex.Split(expression);
+        var splittedExpressions = expression.SplitAccordingTo(expressionSplitRegex);
         return splittedExpressions.Where(subExpression =>
             !string.IsNullOrWhiteSpace(subExpression) &&
-            onlyHasExclamationPointsAndQuestionMarksRegex.IsMatch(subExpression)
+            !subExpression.Matches(onlyHasExclamationPointsAndQuestionMarksRegex)
         );
     }
 }
