e2d12b5810893078d26fb167a8a1d779ee2e91b7
lucaaaum <lucasrenato.fernandesdasilva@gmail.com>
2024-05-01 14:59:05 -0300

    Updating the Main method so we have a try catch block in order to get errors!

--- src/DreamBerdInterpreter.ConsoleApplication/Program.cs
+++ src/DreamBerdInterpreter.ConsoleApplication/Program.cs
@@ -1,10 +1,27 @@
-﻿namespace DreamBerdInterpreter.ConsoleApplication;
+﻿using Cocona;
+using DreamBerdInterpreter.Infrastructure.Console;
+
+namespace DreamBerdInterpreter.ConsoleApplication;
 
 internal class Program
 {
-    public static void Main(string[] args)
-    {
-        var path = args[0];
-        
-    }
+    public static void Main() =>
+        CoconaApp.Run(([Argument] string path) =>
+        {
+            if (!File.Exists(path))
+                Console.WriteLine("Bro I can't run this, the path is invalid!");
+            var fileContent = string.Empty;
+            try
+            {
+                fileContent = File.ReadAllText(path);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("sumthing happend idunno");
+                Console.WriteLine("here's the log if you know dotnet:");
+                Console.WriteLine(e);
+            }
+            var interpreter = new Interpreter.Interpreter(new ConsoleWrapper());
+            interpreter.Interpret(fileContent);
+        });
 }
