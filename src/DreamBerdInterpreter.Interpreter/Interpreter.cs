71b7a8b63ce7fef410110fd48fd584f296fcf64d
lucaaaum <lucasrenato.fernandesdasilva@gmail.com>
2024-05-01 13:16:37 -0300

    Removing commented out lines

--- src/DreamBerdInterpreter.Interpreter/Interpreter.cs
+++ src/DreamBerdInterpreter.Interpreter/Interpreter.cs
@@ -1,3 +1,4 @@
+using System.Text;
 using DreamBerdInterpreter.Infrastructure.Console;
 
 namespace DreamBerdInterpreter.Interpreter;
@@ -8,6 +9,16 @@ public class Interpreter(IConsole console)
 
     public void Interpret(string fileContent)
     {
-        
+        var lines = fileContent.Split(Environment.NewLine);
+        var linesNotCommentedOut = lines.Where(line => !line.StartsWith("//"));
+        fileContent = BuildStringFromLines(lines);
+    }
+
+    public static string BuildStringFromLines(IEnumerable<string> lines)
+    {
+        var stringBuilder = new StringBuilder();
+        foreach (var line in lines)
+            stringBuilder.AppendLine(line);
+        return stringBuilder.ToString();
     }
 }
