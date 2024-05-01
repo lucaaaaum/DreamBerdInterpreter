6d97e965a88ee82d2cbb71c4f51c5b953f080132
lucaaaum <lucasrenato.fernandesdasilva@gmail.com>
2024-05-01 13:06:36 -0300

    Creating the test

--- test/DreamBerdInterpreter.Interpreter.Tests/DreamBerdInterpreter.Interpreter.Tests/InterpreterTests.cs
+++ test/DreamBerdInterpreter.Interpreter.Tests/DreamBerdInterpreter.Interpreter.Tests/InterpreterTests.cs
@@ -1,15 +1,26 @@
+using DreamBerd.Infrastructure.Tests;
+
 namespace DreamBerdInterpreter.Interpreter.Tests;
 
 public class InterpreterTests
 {
+    private string _fileContent;
+    
     [SetUp]
-    public void Setup()
+    public async Task Setup()
     {
+        var cancellationTokenSource = new CancellationTokenSource();
+        var cancellationToken = cancellationTokenSource.Token;
+        _fileContent = await File.ReadAllTextAsync("first_script.dreamberd", cancellationToken);
+        cancellationTokenSource.Dispose();
     }
 
     [Test]
-    public void Test1()
+    public void Test_Output()
     {
-        Assert.Pass();
+        var fakeConsole = new FakeConsole();
+        var interpreter = new Interpreter(fakeConsole);
+        interpreter.Interpret(_fileContent);
+        Assert.That(fakeConsole.Output, Is.EqualTo("batatinha"));
     }
 }
