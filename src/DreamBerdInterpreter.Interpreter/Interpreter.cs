b50da2594e4bdd80316c49e7d2ff0c2b672b4912
lucaaaum <lucasrenato.fernandesdasilva@gmail.com>
2024-05-01 18:13:59 -0300

    Creating some logic to use functions and so on (the printing is fucking working and i dont even know how)

--- src/DreamBerdInterpreter.Interpreter/Interpreter.cs
+++ src/DreamBerdInterpreter.Interpreter/Interpreter.cs
@@ -11,19 +11,52 @@ public class Interpreter(IConsole console)
     private IDictionary<string, VarConst> _variables = new Dictionary<string, VarConst>();
     private IDictionary<string, Function> _functions = new Dictionary<string, Function>();
 
-    private Regex expressionSplitRegex = new(@"([!?]+)(?![^{]*})|}", RegexOptions.Compiled);
-    private Regex onlyHasExclamationPointsAndQuestionMarksRegex = new("^[!?]+$", RegexOptions.Compiled);
-    private Regex varConstRegex = new(@"(var|const)\s*(var|const)\s*([^\s]*)\s*=*\s*([^!?]*)", RegexOptions.Compiled);
+    private readonly Regex _expressionSplitRegex = new(
+        @"([!?]+)(?![^{]*})|}",
+        RegexOptions.Compiled
+    );
+
+    private readonly Regex _onlyHasExclamationPointsAndQuestionMarksRegex = new(
+        "^[!?]+$",
+        RegexOptions.Compiled
+    );
+
+    private readonly Regex _varConstRegex = new(
+        @"(var|const)\s*(var|const)\s*([^\s]*)\s*=*\s*([^!?]*)",
+        RegexOptions.Compiled
+    );
+
+    private readonly Regex _printRegex = new(
+        @"print\((.*)\)",
+        RegexOptions.Compiled
+    );
+
+    private Regex _functionCallRegex = new(
+        @"(.*)\((.*)\)",
+        RegexOptions.Compiled
+    );
 
     public string Interpret(string expression, int visibilityLevel = 0)
     {
         var subExpressions = GetSubExpressions(expression);
 
         foreach (var subExpression in subExpressions)
-            if (subExpression.Matches(varConstRegex))
+            if (subExpression.Matches(_varConstRegex))
             {
                 var variable = GetVarConstFromSubExpression(visibilityLevel, subExpression);
                 _variables.Add(variable.Name, variable);
+            }
+            else if (subExpression.Matches(_printRegex))
+            {
+                var match = _printRegex.Match(subExpression);
+                var textToPrint = match.Groups[1].ToString();
+                _console.WriteLine(textToPrint);
+            }
+            else if (subExpression.Matches(_functionCallRegex))
+            {
+                var match = _functionCallRegex.Match(subExpression);
+                var function = _functions[match.Groups[1].ToString()];
+                Interpret(function.Expression, visibilityLevel + 1);
             }
 
         return default;
@@ -31,7 +64,7 @@ public class Interpreter(IConsole console)
 
     private VarConst GetVarConstFromSubExpression(int visibilityLevel, string subExpression)
     {
-        var match = varConstRegex.Match(subExpression);
+        var match = _varConstRegex.Match(subExpression);
         var reassignable = match.Groups[1].ToString() == "var";
         var editable = match.Groups[2].ToString() == "var";
         var name = match.Groups[3].ToString();
@@ -42,10 +75,10 @@ public class Interpreter(IConsole console)
 
     private IEnumerable<string> GetSubExpressions(string expression)
     {
-        var splittedExpressions = expression.SplitAccordingTo(expressionSplitRegex);
+        var splittedExpressions = expression.SplitAccordingTo(_expressionSplitRegex);
         return splittedExpressions.Where(subExpression =>
             !string.IsNullOrWhiteSpace(subExpression) &&
-            !subExpression.Matches(onlyHasExclamationPointsAndQuestionMarksRegex)
+            !subExpression.Matches(_onlyHasExclamationPointsAndQuestionMarksRegex)
         );
     }
 }
